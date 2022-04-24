b9e50b4d5b7de0419e245aed916738c34f635806
Ximena Calderón <86806702+ximecalderon@users.noreply.github.com>
2022-04-24 18:56:09 -0500

    User + Post setup

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
-const { MONGODB } = require('config.js')
+const Post = require('./models')
+const { MONGODB } = require('./config.js')
 
 const typeDefs = gql`
 	type Query {
